a7b2e77e70f85e56023195d60062b471126db4e2
Oscar Interview <oscarinterview@Oscar-Interview.local>
2015-06-08 16:37:21 -0400

    underscore

--- questions/front-end/template/template.js
+++ questions/front-end/template/template.js
@@ -15,5 +15,56 @@
     // The variable "ret" would contain the string "hello oscar".
     //
     // Fill in your template function here.
+    
+    /*
+    "world": [
+        {
+            "start": ...
+            "end": ...
+        },
+        {
+        
+        }
+    ]
+    */
+    var keys = {
+
+
+    };
+
+    var markerStart = "<%=";
+    var markerEnd = "%>";
+    var cursor = 0;
+    var startPos = 0;
+    var stopCond = 1;
+    while (stopCond > 0) {
+        cursor = templateString.indexOf(markerStart, startPos);
+
+        endPosition = templateString.indexOf(markerEnd, cursor);
+        inBetween = templateString.substr(cursor+3, endPosition-cursor-3);
+        inBetween = inBetween.trim();
+        
+        if (!keys[inBetween]) {
+            keys[inBetween] = [];
+        }
+        
+        keys[inBetween].push({
+            start: cursor,
+            end: endPosition
+        });
+        console.log(inBetween, keys[inBetween]);
+        startPos = endPosition;
+        stopCond--;
+    }
+    return function(params) { 
+        var result = templateString;
+        for(var key in params) {
+            var value = params[key];
+            var locations = keys[key];
+            locations.forEach(function(config) {
+
+            });
+        }
+    };
   };
 })()
