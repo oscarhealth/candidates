557f6ebbffcbfe6694e32c2ecf41e186bb5fd816
Oscar Code <code@hioscar.com>
2015-08-06 16:48:04 -0400

    UTF8

--- questions/back_end/utf8/utf8_todo.java
+++ questions/back_end/utf8/utf8_todo.java
@@ -11,18 +11,35 @@ Bits of code point  First code point  Last code point     Byte 1   Byte 2   Byte
 11          U+0080        U+07FF        110xxxxx 10xxxxxx
 16          U+0800        U+FFFF        1110xxxx 10xxxxxx 10xxxxxx
 21          U+10000       U+1FFFFF      11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-26          U+200000      U+3FFFFFF       111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
-31          U+4000000       U+7FFFFFFF      1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+26          U+200000      U+3FFFFFF     111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+31          U+4000000     U+7FFFFFFF    1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
 
 */
 
 public class utf8_todo {
   static void reverseBytes(byte[] bytes, int startingOffset, int len) {
-    // TODO implement me
+  
+    for (int i = startingOffset; i < len ; i++) {
+        
+        byte b = bytes[len - i];
+
+    }
+
   }
 
   static void reverseUTF8Bytes(byte[] bytes) {
-    // TODO implement me
+      for (int i = 0; i < bytes.length ; i++) {
+
+
+          switch(bytes[i]) {
+            case : bytes[i] & 0x40 == 0;
+              // log error
+              break;
+              case : bytes[i] & C0 == 0;
+                reverseBytes(bytes, i, 2)
+              break;
+          } 
+      }
   }
 
 
